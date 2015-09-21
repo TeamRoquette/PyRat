8505ac06a21c7801bc17b81cf94a35e36c6db8cc
Jacquin Théo <nimag42@Giranboule.yolo>
2015-09-21 14:36:05 +0200

    Correction du bug pour l'algo de dijkstra

--- dijsktra_bot.py
+++ dijsktra_bot.py
@@ -72,6 +72,7 @@ def processNextInformation () :
 
 
 
+path = []
 last_directions = []
 last_positions = []
 
@@ -80,12 +81,12 @@ last_positions = []
 def ordonne (best_vert, start, stop, path):
     if start == stop:
         return [start] + path
-    debug (best_vert)
+
     return ordonne (best_vert, start, best_vert[stop][0], [stop] + path) 
 
 
 def dijkstra (mazeMap, playerLocation, coinLocation) :
-    best_vertice = {}
+    best_vertice = {(playerLocation):((),0)}
     seen_vertice = []
     to_see_vertice = [playerLocation]
     #while len(seen_vertice) != len(mazeMap) :
@@ -96,13 +97,12 @@ def dijkstra (mazeMap, playerLocation, coinLocation) :
             dist = best_vertice.get(vertex, ([], float('inf')))[1]
             
             for (v,d) in voisins :
-                debug (v)
                 if best_vertice.get(v, ([], float('inf')))[1] > d + dist :
                     best_vertice[v] = (vertex, d + dist)
                     
                 seen_vertice.append(vertex)
                 to_see_vertice.append(v)
-    debug(best_vertice)
+
     return ordonne(best_vertice, playerLocation, coinLocation, [])
 
 
@@ -111,31 +111,38 @@ def reltoabs_direction (rel_direction, abs_direction):
     return (rel_direction + abs_direction) % 4
 
 
+def nextpostodir (next_pos, actual_pos, mazeMap):
+    # Check if next position is reachable
+    next_poses = mazeMap[actual_pos]
+    if next_pos not in next_poses:
+        return -1
+
+    (x_act, y_act) = actual_pos
+    (x_next, y_next) = next_pos
+
+    #if x_act == x_next:
+        #if :
+    
+
+
 def initializationCode (mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
 
-    path = dijkstra (mazeMap, playerLocation, coins[0])
-    last_directions.append(DOWN)
-    last_positions.append(playerLocation)
+    global path
 
-    debug(path)
+    path = dijkstra (mazeMap, playerLocation, coins[0])
+    debug (path)
+    st = path.pop()
+    
+    if (st != playerLocation):
+        return "BUG!"
+    return "Everything seems fine, let's start !"
 
 
 def determineNextMove (mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
 
-    last_dir = last_directions[-1]
-    
-    if (playerLocation == last_positions[-1]):
-        # On n'a pas pu bouger
-        direction = reltoabs_direction (LEFT, last_dir);
-
-    # C'est la première fois qu'on arriv sur cette case
-    else :
-    	direction = reltoabs_direction (RIGHT, last_dir)
-        
-    # On ajoute en mémoire notre décision
-    last_positions.append(playerLocation)
-    last_directions.append(direction)
-    return CONV_KEY[direction]
+    global path
+    nextPos = path.pop()
+    return 
 
 
 
