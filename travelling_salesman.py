dff9091e168b3b995403be3c6e30791d0634570f
Theo JACQUIN <tjacquin@pc-df-575.priv.enst-bretagne.fr>
2015-10-02 18:15:58 +0200

    Implementatiion d'une heuristique tres simple poutr le probleme du voyageur

--- travelling_salesman.py
+++ travelling_salesman.py
@@ -73,19 +73,21 @@ def processNextInformation () :
 ##############
 
 
-
+moving = False
 path = []
 last_directions = []
 last_positions = []
-
+eaten_coins = []
+meta_graphe = {}
+meilleurs_chemins = {}
 
 # Fonction intermédiaire pour parcours_en_largeur
 # trie le dictionnaire des meilleurs sommets pour en faire une liste compréhensible plus facilement
-def ordonne (best_vert, start, stop, path, dist):
+def ordonne (best_vert, start, stop, path):
     if start == stop:
-        return path + [start], dist
+        return path + [start]
 
-    return ordonne (best_vert, start, best_vert[stop][0], path + [stop], dist+best_vert[stop][1]) 
+    return ordonne (best_vert, start, best_vert[stop][0], path + [stop]) 
 
 
 
@@ -104,29 +106,7 @@ def dijkstra (mazeMap, startLocation, stopLocation) :
                 best_vertice[v] = (vertex, d + dist)
                 to_see_vertice.append(v)
 
-    return ordonne(best_vertice, startLocation, stopLocation, [], 0)
-
-
-
-def debugmap(best_vertice,height_map):
-    l = ''
-    for i in range (height_map):
-        for j in range (height_map):
-            c = best_vertice.get((i,j), ([], -1) )[1]
-            if c == -1:
-                l += '---'
-            elif c < 10:
-                l += ' '+str(c)+' '
-            elif c < 100:
-                l += ' '+str(c)
-            else:
-                l += str(c)
-                
-            l+= ' '
-
-        l+='\n'
-            
-    debug(l+'\n\n\n')
+    return ordonne(best_vertice, startLocation, stopLocation, []), best_vertice[stopLocation][1]
 
 
 
@@ -156,38 +136,99 @@ def nextpostodir (next_pos, actual_pos, mazeMap):
             return RIGHT
         else:
             return LEFT
-    
 
 
-def initializationCode (mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
-    to = time.time()
-    global path
 
-    # Construction d'un meta-graphe
+def make_meta_graph (mazeMap, playerLocation, coins):
     sommets = [playerLocation] + coins
     
-    meta_graphe = {}
-    meilleurs_chemins = {}
-
+    meta_graph = {}
+    best_ways = {}
     for i in sommets:
         for j in sommets:
             if j != i:
+
                 chemin, distance = dijkstra(mazeMap, i,j)
-                meilleurs_chemins[i].append((j,chemin))
-                meta_graphe[i].append((j, distance))
+                if i not in best_ways :
+                    best_ways[i] = {}
+                    meta_graph[i] = {}
+
+                if j not in best_ways :
+                    best_ways[j] = {}
+                    meta_graph[j] = {}
+
+                meta_graph[i][j]= distance
+                best_ways[i][j] = chemin
+
+                meta_graph[j][i]= distance
+                best_ways[j][i] = chemin[::-1]
+
 
         sommets.remove(i)
+    
+    return meta_graph, best_ways
+
+
+
+def update_coins (eaten_coins, elLocation):
+
+    if elLocation in meta_graphe:
+        eaten_coins.append(elLocation)
+    
+    return eaten_coins
+
+
+    
+def initializationCode (mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
+    to = time.time()
+
+    global meta_graphe
+    global meilleurs_chemins
+
+    # Construction d'un meta-graphe
+    meta_graphe, meilleurs_chemins = make_meta_graph(mazeMap, playerLocation, coins)
 
     elapsed = time.time() - to
-    debug(elapsed)
-
-    debug(meta_graphe)
-
+    debug(elapsed) 
+    
     return "Everything seems fine, let's start !"
+
 
 
 def determineNextMove (mazeWidth, mazeHeight, mazeMap, timeAllowed, playerLocation, opponentLocation, coins) :
     global path
+    global moving
+    global meta_graphe
+    global meilleurs_chemins
+    global eaten_coins
+
+    # Si l'ennemi mange une pièce en chocolat, on la comp(o)te
+    eaten_coins = update_coins(eaten_coins, opponentLocation)
+
+    if moving: 
+        if not path :
+            moving = False
+
+    # Si on n'a plus d'objectif, on cherche la plus proche pièce
+    if not moving :
+
+        # On choisit la prochaine plus proche pièce
+        plusprochepiece = ((-1,-1),float('inf'))
+        for locationCoin in meta_graphe[playerLocation]:
+            if meta_graphe[playerLocation][locationCoin] < plusprochepiece[1] and locationCoin not in eaten_coins  :
+                plusprochepiece = (locationCoin, meta_graphe[playerLocation][locationCoin])
+
+        # On récupère le chemin pour aller a cette pièce
+
+        path = meilleurs_chemins[playerLocation][plusprochepiece[0]]
+        
+        path.pop() # on enlève le premier élt qui est la position actuelle
+
+        # Si on a mangé une pièce en chocolat, on la comp(o)te
+        eaten_coins = update_coins(eaten_coins, playerLocation)
+
+        moving = True
+                
     nextPos = path.pop()
     nextDir = CONV_KEY[nextpostodir(nextPos, playerLocation, mazeMap)] 
 
